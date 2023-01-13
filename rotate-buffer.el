1ad453b7bd4c0a23c70ec3b3517262ad3b23ecf3
sh-shota <shota-i@kwasnei.ac.jp>
2023-01-14 05:04:41 +0900

    add file

---
+++ rotate-buffer.el
@@ -0,0 +1,26 @@
+(defun right-rotate-buffer()
+  "M-] for right rotate visble buffers"
+  (interactive)
+  (setq buffers (cons (window-buffer) `()))
+  (setq windows (cons (selected-window) `()))
+  (setq itr 0)
+
+  (while (not (eq (nth itr windows) (next-window (nth 0 windows))))
+    (setq itr (1+ itr))
+    (setq windows (cons (next-window (nth 0 windows)) windows))
+    (setq buffers (cons (window-buffer (nth 0 windows)) buffers))
+    )
+  (setq num itr)
+
+  (setq windows (reverse windows))
+  (setq newbuff (cons (pop buffers) `()))
+  (setq newbuff (append newbuff (reverse buffers)))
+  
+  (setq itr 0)
+  (while (<= itr num)
+    (set-window-buffer (nth itr windows) (nth itr newbuff))
+    (setq itr (1+ itr))
+    )
+  )
+
+(global-set-key "\M-]" `right-rotate-buffer)
