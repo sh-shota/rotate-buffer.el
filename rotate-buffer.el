47a370fa878c37fb69cf8486be3eae287f41980f
shota <eyh49147@kwansei.ac.jp>
2023-01-14 18:56:40 +0900

    add left-rortate

--- rotate-buffer.el
+++ rotate-buffer.el
@@ -23,4 +23,28 @@
     )
   )
 
-(global-set-key "\M-]" `right-rotate-buffer)
+(defun left-rotate-buffer()
+  "M-[ for right rotate visble buffers"
+  (interactive)
+  (setq buffers (cons (window-buffer) `()))
+  (setq windows (cons (selected-window) `()))
+  (setq itr 0)
+
+  (while (not (eq (nth itr windows) (next-window (nth 0 windows))))
+    (setq itr (1+ itr))
+    (setq windows (cons (next-window (nth 0 windows)) windows))
+    (setq buffers (cons (window-buffer (nth 0 windows)) buffers))
+    )
+  (setq num itr)
+
+  (setq windows (reverse windows))
+  (setq revbuff (reverse buffers))
+  (setq newbuff (cons (pop revbuff) `()))
+  (setq newbuff (append  revbuff newbuff))
+  
+  (setq itr 0)
+  (while (<= itr num)
+    (set-window-buffer (nth itr windows) (nth itr newbuff))
+    (setq itr (1+ itr))
+    )
+  )
